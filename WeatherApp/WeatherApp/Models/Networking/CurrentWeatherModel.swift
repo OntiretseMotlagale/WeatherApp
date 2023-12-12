9e67322d6c91eb5b79829a40a35a273d834af867
Ontiretse-Glucode <Ontiretse@glucode.com>
2023-12-12 12:26:58 +0200

    Added the Service Models

---
+++ WeatherApp/WeatherApp/Models/Networking/CurrentWeatherModel.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+
+struct CurrentWeatherModel: Codable, Equatable {
+    static func == (lhs: CurrentWeatherModel, rhs: CurrentWeatherModel) -> Bool {
+        return lhs.name == rhs.name
+    }
+    
+    var coord: Coordinates?
+    var name: String?
+    var main: MainData?
+    var weather: [WeatherData]?
+}
+
+struct WeatherData: Codable {
+    var main: String?
+    var description: String?
+}
+
+struct Coordinates: Codable {
+    var lon: Float?
+    var lat: Float?
+}
+
+struct MainData: Codable {
+    var temp_max: Double?
+    var temp: Double?
+    var feels_like: Double?
+    var temp_min: Double?
+}
+
+struct Forecast: Codable {
+    var dt_txt: String?
+    var main: MainData?
+    var weather: [WeatherData]?
+}
+
+struct ForecastWeatherResponse: Codable {
+    var list: [Forecast]?
+}
