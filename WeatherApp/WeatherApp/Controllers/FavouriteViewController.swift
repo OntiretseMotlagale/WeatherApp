d9a90ae84a7dea8412336e61080e84e636e3fdb7
Ontiretse-Glucode <Ontiretse@glucode.com>
2023-12-14 09:10:13 +0200

    Created the FavouriteViewController

---
+++ WeatherApp/WeatherApp/Controllers/FavouriteViewController.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+class FavouriteViewController: UIViewController {
+    
+    var delegate: MainTabBarDelegate?
+    var viewModel = FavouritesViewModel()
+    
+    @IBOutlet weak var favouriteTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        favouriteTableView.delegate = self
+        favouriteTableView.dataSource = self
+        registerCell()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        setupView()
+    }
+    func registerCell() {
+        favouriteTableView.register(UINib(nibName: "FavouritesTableViewCell", bundle: nil), forCellReuseIdentifier: "FavouritesTableViewCell")
+    }
+    func setupView() {
+        viewModel.resetFavorites()
+        if viewModel.favoriteListCount == 0 {
+            //            NoFavouritesText.text = "No favorite weather locations"
+            favouriteTableView.isHidden = true
+        } else {
+            favouriteTableView.isHidden = false
+            favouriteTableView.reloadData()
+        }
+    }
+}
+
+extension FavouriteViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.favoriteListCount
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavouritesTableViewCell", for: indexPath) as! FavouritesTableViewCell
+        cell.favouriteWeatherLocation.text = viewModel.favoritesList[indexPath.row].name
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedWeatherLocation =  viewModel.favoritesList[indexPath.row]
+        delegate?.transitionToHomeScreen(with: selectedWeatherLocation)
+    }
+}
