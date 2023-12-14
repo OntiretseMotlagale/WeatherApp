0a71eab7ffbffcc2f9d3862f5ce2d6bbdd885e42
Ontiretse-Glucode <Ontiretse@glucode.com>
2023-12-14 09:46:58 +0200

    Added Favourites Cell

---
+++ WeatherApp/WeatherApp/Views/xibs/Favourites/FavouritesTableViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class FavouritesTableViewCell: UITableViewCell {
+    @IBOutlet weak var favouriteWeatherLocation: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
